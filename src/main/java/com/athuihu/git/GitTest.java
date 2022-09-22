21ca6c4f302bcfcd44a35b14dd94c1513898d73a
yujiahui <952982660@qq.com>
2022-09-22 17:40:39 +0800

    first commit

---
+++ src/main/java/com/athuihu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.athuihu.git;
+
+
+/*
+ * @Author:余佳辉
+ * @Description:
+ *
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
