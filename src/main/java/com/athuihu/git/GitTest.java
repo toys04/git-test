f70de8c54200a60c34101ea83de8c234112b9e10
yujiahui <952982660@qq.com>
2022-09-26 11:28:16 +0800

    马云push

--- src/main/java/com/athuihu/git/GitTest.java
+++ src/main/java/com/athuihu/git/GitTest.java
@@ -16,5 +16,6 @@ public class GitTest {
         System.out.println("hot-fix commit");
         System.out.println("push commit");
         System.out.println("pull commit");
+        System.out.println("mayu gitee push");
     }
 }
